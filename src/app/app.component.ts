8ff3632cd34d508d721b9c1d098815e569c8cbae
Sameen Hewage <sameen.android@gmail.com>
2023-01-03 22:58:48 +0530

    added services to get the title name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private sharedService: SharedService,
+    private titleService: Title,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.sharedService.blogTitle);
+  }
   title = 'blog';
 }
